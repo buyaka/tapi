988bed6f608764b45e3db02521644361e0cbf890
buyaka <buyantorgil@gmail.com>
2016-03-14 06:04:44 +0000

    Added catch undefined route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
 
   def index
-    render :json => '{ msg: "Are you ready for api battle :)" }'
+    render :json => '{ "msg" : "Are you ready for api battle :)" }'
   end
   
   def catch_404
-    render :json => '{ msg: "The route is not defined! :p" }'
+    render :json => '{ "msg" : "The route is not defined! :p" }'
   end
 
 end
